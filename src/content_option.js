0a063e36eff42ac8da43144ead7cd73dae8f7fdd
Prasanta Panda <prasantpanda033@gmail.com>
2023-05-15 12:19:00 +0530

    adding new files

---
+++ src/content_option.js
@@ -0,0 +1,152 @@
+const logotext = "Prasanta Panda";
+const meta = {
+    title: "Prasanta Panda",
+    description: "I’m Prasant Panda backend devloper,currently working in Bhubaneswar",
+};
+
+const introdata = {
+    title: "I’m Prasanta Panda",
+    animated: {
+        first: "I love react js",
+        second: "I also Know java",
+        third: "I develop web services",
+    },
+    description: "I have total 3 year experience in java and spring boot",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I am a fullstack developer with proficiency in java, spring boot and micro services. I am learning react js",
+};
+const worktimeline = [{
+        jobtitle: "Orissa Doot Private Limited",
+        where: "Bhubaneswar",
+        date: "2019 - 2021",
+    },
+    {
+        jobtitle: "Black Knight India Solutions",
+        where: "Bhubaneswar",
+        date: "2021 - 2022",
+    },
+   
+];
+
+const skills = [{
+        name: "java",
+        value: 90,
+    },
+    {
+        name: "unix",
+        value: 80,
+    },
+    {
+        name: "mysql",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 30,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [
+       
+    
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://www.freeimages.com/photo/sunflower-in-bulgaria-1641774",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.freeimages.com/vhq/images/previews/083/cool-heraldic-lion-shield-vector-31867.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "prasantpanda033@gmail.com",
+    YOUR_FONE: "917539015558",
+    description: ": Permanent address :-  Plot no. 556B, Unit - 9, Bayababa Math road, Bhubaneswar, 751022",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    linkedin: "https://www.linkedin.com/in/prasantpanda033",
+    facebook: "https://www.facebook.com/prasanta.panda.3954/",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
